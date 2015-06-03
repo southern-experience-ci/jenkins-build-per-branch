a68a7ad4c31a94ee017a9707ab0290d0cfe11893
dbpfindexp <dbpatel@findexp.com>
2015-06-03 12:39:52 -0400

    Revert variable to use templateJobPrefix

--- src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
@@ -12,8 +12,6 @@ class JenkinsJobManager {
     String jenkinsUser
     String jenkinsPassword
     String latestRegex
-    String genericTemplateName
-
     Boolean dryRun = false
     Boolean noViews = false
     Boolean noDelete = false
@@ -143,7 +141,7 @@ class JenkinsJobManager {
     }
 
     List<TemplateJob> findRequiredTemplateJobs(List<String> allJobNames) {
-        String regex = /^($genericTemplateName-[^-]*)-($templateBranchName)$/
+        String regex = /^($templateJobPrefix-[^-]*)-($templateBranchName)$/
 
         List<TemplateJob> templateJobs = allJobNames.findResults { String jobName ->
             TemplateJob templateJob = null
