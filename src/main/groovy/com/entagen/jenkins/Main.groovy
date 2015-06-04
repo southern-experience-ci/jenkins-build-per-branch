64056cc478bafb3c3593544ea30ec61c99ebd4a9
Joshua Collins <joshua.brent.collins@gmail.com>
2015-06-04 14:22:35 -0400

    Update Main.groovy
    Add Arg for generic template prefix

--- src/main/groovy/com/entagen/jenkins/Main.groovy
+++ src/main/groovy/com/entagen/jenkins/Main.groovy
@@ -11,6 +11,7 @@ class Main {
             u: [longOpt: 'git-url',  required: true, args: 1, argName: 'gitUrl', description: "Git Repository URL - gradle flag -DgitUrl=<gitUrl>"],
             p: [longOpt: 'job-prefix', required: true, args: 1, argName: 'templateJobPrefix', description: "Template Job Prefix, - gradle flag -DtemplateJobPrefix=<jobPrefix>"],
             t: [longOpt: 'template-branch', required: true, args: 1, argName:  'templateBranchName', description: "Template Branch Name - gradle flag -DtemplateBranchName=<branchName>"],
+            g: [longOpt: 'template-prefix', required: true, args: 1, argName:  'genericTemplateName', description: "Generic Template Prefix - gradle flag -DgenericTemplateName=<template prefix>"],
             n: [longOpt: 'nested-view', required: false, args: 1, argName: 'nestedView', description: "Nested Parent View Name - gradle flag -DnestedView=<nestedView> - optional - must have Jenkins Nested View Plugin installed"],
             c: [longOpt: 'print-config', required: false, args: 0, argName: 'printConfig', description:  "Check configuration - print out settings then exit - gradle flag -DprintConfig=true"],
             d: [longOpt: 'dry-run', required: false, args: 0, argName: 'dryRun', description:  "Dry run, don't actually modify, create, or delete any jobs, just print out what would happen - gradle flag: -DdryRun=true"],
